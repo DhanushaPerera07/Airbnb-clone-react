84f5453bb19ed4ac353c824418188e0042662176
DhanushaPerera07 <buddhika.dhanusha@gmail.com>
2021-02-16 19:40:06 +0530

    Create app/Banner.js component

---
+++ src/app/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Banner() {
+    return (
+        <div className="banner">
+            <h1>Hello I am the banner</h1>
+        </div>
+    )
+}
+
+export default Banner
